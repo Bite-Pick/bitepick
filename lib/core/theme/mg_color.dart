9fd31d55bee20fb4f58e638f6183a45d49e4b763
kimwest00 <kimwest00@hufs.ac.kr>
2025-10-01 16:24:07 +0900

    add: color

---
+++ lib/core/theme/mg_color.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class MgColorScheme {
+  MgColorScheme._();
+
+  // Primary & Secondary
+  static const Color primary = Color(0xFFFFD459);
+  static const Color secondary = Color(0xFF4BB2FF);
+
+  // Sub Point Colors
+  static const Color subpointRed = Color(0xFFEF444D);
+  static const Color subpointGreen = Color(0xFF69D441);
+  static const Color subpointPink = Color(0xFFFF89E1);
+
+  // Base Colors
+  static const Color background = Color(0xFFFFFFFF);
+  static const Color text = Color(0xFF333333);
+  static const Color disabled = Color(0xFFF6F6F6);
+  static const Color gray = Color(0xFF7F7F7F);
+  static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color lightest = Color(0xFFFFF8E6);
+
+  // Gray Scale (11단계)
+  static const Color gray1 = Color(0xFF212121);
+  static const Color gray2 = Color(0xFF424242);
+  static const Color gray3 = Color(0xFF616161);
+  static const Color gray4 = Color(0xFF757575);
+  static const Color gray5 = Color(0xFF9E9E9E);
+  static const Color gray6 = Color(0xFFBDBDBD);
+  static const Color gray7 = Color(0xFFE0E0E0);
+  static const Color gray8 = Color(0xFFEEEEEE);
+  static const Color gray9 = Color(0xFFF5F5F5);
+  static const Color gray10 = Color(0xFFFAFAFA);
+  static const Color gray11 = Color(0xFFFFFFFF);
+
+  // Navigation Colors
+  static const Color navigationPrimary = Color(0xFFFF6B6B);
+  static const Color navigationBackground = Color(0xFFFFFFFF);
+  static const Color navigationCard = Color(0xFFFFFFFF);
+  static const Color navigationText = Color(0xFF000000);
+  static const Color navigationBorder = Color(0xFFE5E5E5);
+  static const Color navigationNotification = Color(0xFFFF6B6B);
+
+  // Social Login Colors
+  static const Color kakaoYellow = Color(0xFFFAE200);
+  static const Color naverGreen = Color(0xFF03C75A);
+  static const Color appleBlack = Color(0xFF000000);
+
+  // Additional Colors
+  static const Color turquoise = Color(0xFF1CD7AE);
+  static const Color lightGray = Color(0xFFF9FAFB);
+  static const Color divider = Color(0xFFF2F4F6);
+  static const Color headerText = Color(0xFF181818);
+  static const Color inactive = Color(0xFF8F8F8F);
+  static const Color kakaoPayYellow = Color(0xFFFFEB00);
+  static const Color alertRed = Color(0xFFFF5149);
+}
