b7af4f7797d36ded4a67953382e6f383dc916d8a
kimwest00 <kimwest00@hufs.ac.kr>
2025-10-03 21:01:34 +0900

    feat: runApp 메서드로 변경, launch.json 수정

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -4,10 +4,13 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:magambell/src/main_app.dart';
 
-void main() {
-  runZonedGuarded(() async {
-    WidgetsFlutterBinding.ensureInitialized();
+Future<void> runMagamBellApp() async {
+  await runZonedGuarded<Future<void>>(
+    () async {
+      WidgetsFlutterBinding.ensureInitialized();
 
-    runApp(const MagambellApp());
-  }, (error, stackTrace) => log(error.toString()));
+      runApp(const MagambellApp());
+    },
+    (error, stackTrace) => log(error.toString()),
+  );
 }
