c175f458d7c3cd4da3648d321119876189a3d1bb
kimwest00 <kimwest00@hufs.ac.kr>
2025-10-11 20:36:08 -0600

    add: store- home list entity

---
+++ lib/src/features/home/domain/entity/good.freezed.dart
@@ -0,0 +1,446 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'good.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+/// @nodoc
+mixin _$Good {
+  String get storeId => throw _privateConstructorUsedError;
+  String get storeName => throw _privateConstructorUsedError;
+  List<String> get imageUrl => throw _privateConstructorUsedError;
+  double get latitude => throw _privateConstructorUsedError;
+  double get longitude => throw _privateConstructorUsedError;
+  double get address => throw _privateConstructorUsedError;
+  double get goodsName => throw _privateConstructorUsedError;
+  String get startTime => throw _privateConstructorUsedError;
+  String get endTime => throw _privateConstructorUsedError;
+  int get originPrice => throw _privateConstructorUsedError;
+  double get discount => throw _privateConstructorUsedError;
+  double get salePrice => throw _privateConstructorUsedError;
+  double get quantity => throw _privateConstructorUsedError;
+  double get distance => throw _privateConstructorUsedError;
+  double get saleStatus => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $GoodCopyWith<Good> get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $GoodCopyWith<$Res> {
+  factory $GoodCopyWith(Good value, $Res Function(Good) then) =
+      _$GoodCopyWithImpl<$Res, Good>;
+  @useResult
+  $Res call(
+      {String storeId,
+      String storeName,
+      List<String> imageUrl,
+      double latitude,
+      double longitude,
+      double address,
+      double goodsName,
+      String startTime,
+      String endTime,
+      int originPrice,
+      double discount,
+      double salePrice,
+      double quantity,
+      double distance,
+      double saleStatus});
+}
+
+/// @nodoc
+class _$GoodCopyWithImpl<$Res, $Val extends Good>
+    implements $GoodCopyWith<$Res> {
+  _$GoodCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? storeId = null,
+    Object? storeName = null,
+    Object? imageUrl = null,
+    Object? latitude = null,
+    Object? longitude = null,
+    Object? address = null,
+    Object? goodsName = null,
+    Object? startTime = null,
+    Object? endTime = null,
+    Object? originPrice = null,
+    Object? discount = null,
+    Object? salePrice = null,
+    Object? quantity = null,
+    Object? distance = null,
+    Object? saleStatus = null,
+  }) {
+    return _then(_value.copyWith(
+      storeId: null == storeId
+          ? _value.storeId
+          : storeId // ignore: cast_nullable_to_non_nullable
+              as String,
+      storeName: null == storeName
+          ? _value.storeName
+          : storeName // ignore: cast_nullable_to_non_nullable
+              as String,
+      imageUrl: null == imageUrl
+          ? _value.imageUrl
+          : imageUrl // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+      latitude: null == latitude
+          ? _value.latitude
+          : latitude // ignore: cast_nullable_to_non_nullable
+              as double,
+      longitude: null == longitude
+          ? _value.longitude
+          : longitude // ignore: cast_nullable_to_non_nullable
+              as double,
+      address: null == address
+          ? _value.address
+          : address // ignore: cast_nullable_to_non_nullable
+              as double,
+      goodsName: null == goodsName
+          ? _value.goodsName
+          : goodsName // ignore: cast_nullable_to_non_nullable
+              as double,
+      startTime: null == startTime
+          ? _value.startTime
+          : startTime // ignore: cast_nullable_to_non_nullable
+              as String,
+      endTime: null == endTime
+          ? _value.endTime
+          : endTime // ignore: cast_nullable_to_non_nullable
+              as String,
+      originPrice: null == originPrice
+          ? _value.originPrice
+          : originPrice // ignore: cast_nullable_to_non_nullable
+              as int,
+      discount: null == discount
+          ? _value.discount
+          : discount // ignore: cast_nullable_to_non_nullable
+              as double,
+      salePrice: null == salePrice
+          ? _value.salePrice
+          : salePrice // ignore: cast_nullable_to_non_nullable
+              as double,
+      quantity: null == quantity
+          ? _value.quantity
+          : quantity // ignore: cast_nullable_to_non_nullable
+              as double,
+      distance: null == distance
+          ? _value.distance
+          : distance // ignore: cast_nullable_to_non_nullable
+              as double,
+      saleStatus: null == saleStatus
+          ? _value.saleStatus
+          : saleStatus // ignore: cast_nullable_to_non_nullable
+              as double,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$GoodImplCopyWith<$Res> implements $GoodCopyWith<$Res> {
+  factory _$$GoodImplCopyWith(
+          _$GoodImpl value, $Res Function(_$GoodImpl) then) =
+      __$$GoodImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {String storeId,
+      String storeName,
+      List<String> imageUrl,
+      double latitude,
+      double longitude,
+      double address,
+      double goodsName,
+      String startTime,
+      String endTime,
+      int originPrice,
+      double discount,
+      double salePrice,
+      double quantity,
+      double distance,
+      double saleStatus});
+}
+
+/// @nodoc
+class __$$GoodImplCopyWithImpl<$Res>
+    extends _$GoodCopyWithImpl<$Res, _$GoodImpl>
+    implements _$$GoodImplCopyWith<$Res> {
+  __$$GoodImplCopyWithImpl(_$GoodImpl _value, $Res Function(_$GoodImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? storeId = null,
+    Object? storeName = null,
+    Object? imageUrl = null,
+    Object? latitude = null,
+    Object? longitude = null,
+    Object? address = null,
+    Object? goodsName = null,
+    Object? startTime = null,
+    Object? endTime = null,
+    Object? originPrice = null,
+    Object? discount = null,
+    Object? salePrice = null,
+    Object? quantity = null,
+    Object? distance = null,
+    Object? saleStatus = null,
+  }) {
+    return _then(_$GoodImpl(
+      storeId: null == storeId
+          ? _value.storeId
+          : storeId // ignore: cast_nullable_to_non_nullable
+              as String,
+      storeName: null == storeName
+          ? _value.storeName
+          : storeName // ignore: cast_nullable_to_non_nullable
+              as String,
+      imageUrl: null == imageUrl
+          ? _value._imageUrl
+          : imageUrl // ignore: cast_nullable_to_non_nullable
+              as List<String>,
+      latitude: null == latitude
+          ? _value.latitude
+          : latitude // ignore: cast_nullable_to_non_nullable
+              as double,
+      longitude: null == longitude
+          ? _value.longitude
+          : longitude // ignore: cast_nullable_to_non_nullable
+              as double,
+      address: null == address
+          ? _value.address
+          : address // ignore: cast_nullable_to_non_nullable
+              as double,
+      goodsName: null == goodsName
+          ? _value.goodsName
+          : goodsName // ignore: cast_nullable_to_non_nullable
+              as double,
+      startTime: null == startTime
+          ? _value.startTime
+          : startTime // ignore: cast_nullable_to_non_nullable
+              as String,
+      endTime: null == endTime
+          ? _value.endTime
+          : endTime // ignore: cast_nullable_to_non_nullable
+              as String,
+      originPrice: null == originPrice
+          ? _value.originPrice
+          : originPrice // ignore: cast_nullable_to_non_nullable
+              as int,
+      discount: null == discount
+          ? _value.discount
+          : discount // ignore: cast_nullable_to_non_nullable
+              as double,
+      salePrice: null == salePrice
+          ? _value.salePrice
+          : salePrice // ignore: cast_nullable_to_non_nullable
+              as double,
+      quantity: null == quantity
+          ? _value.quantity
+          : quantity // ignore: cast_nullable_to_non_nullable
+              as double,
+      distance: null == distance
+          ? _value.distance
+          : distance // ignore: cast_nullable_to_non_nullable
+              as double,
+      saleStatus: null == saleStatus
+          ? _value.saleStatus
+          : saleStatus // ignore: cast_nullable_to_non_nullable
+              as double,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$GoodImpl implements _Good {
+  const _$GoodImpl(
+      {required this.storeId,
+      required this.storeName,
+      required final List<String> imageUrl,
+      required this.latitude,
+      required this.longitude,
+      required this.address,
+      required this.goodsName,
+      required this.startTime,
+      required this.endTime,
+      required this.originPrice,
+      required this.discount,
+      required this.salePrice,
+      required this.quantity,
+      required this.distance,
+      required this.saleStatus})
+      : _imageUrl = imageUrl;
+
+  @override
+  final String storeId;
+  @override
+  final String storeName;
+  final List<String> _imageUrl;
+  @override
+  List<String> get imageUrl {
+    if (_imageUrl is EqualUnmodifiableListView) return _imageUrl;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_imageUrl);
+  }
+
+  @override
+  final double latitude;
+  @override
+  final double longitude;
+  @override
+  final double address;
+  @override
+  final double goodsName;
+  @override
+  final String startTime;
+  @override
+  final String endTime;
+  @override
+  final int originPrice;
+  @override
+  final double discount;
+  @override
+  final double salePrice;
+  @override
+  final double quantity;
+  @override
+  final double distance;
+  @override
+  final double saleStatus;
+
+  @override
+  String toString() {
+    return 'Good(storeId: $storeId, storeName: $storeName, imageUrl: $imageUrl, latitude: $latitude, longitude: $longitude, address: $address, goodsName: $goodsName, startTime: $startTime, endTime: $endTime, originPrice: $originPrice, discount: $discount, salePrice: $salePrice, quantity: $quantity, distance: $distance, saleStatus: $saleStatus)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$GoodImpl &&
+            (identical(other.storeId, storeId) || other.storeId == storeId) &&
+            (identical(other.storeName, storeName) ||
+                other.storeName == storeName) &&
+            const DeepCollectionEquality().equals(other._imageUrl, _imageUrl) &&
+            (identical(other.latitude, latitude) ||
+                other.latitude == latitude) &&
+            (identical(other.longitude, longitude) ||
+                other.longitude == longitude) &&
+            (identical(other.address, address) || other.address == address) &&
+            (identical(other.goodsName, goodsName) ||
+                other.goodsName == goodsName) &&
+            (identical(other.startTime, startTime) ||
+                other.startTime == startTime) &&
+            (identical(other.endTime, endTime) || other.endTime == endTime) &&
+            (identical(other.originPrice, originPrice) ||
+                other.originPrice == originPrice) &&
+            (identical(other.discount, discount) ||
+                other.discount == discount) &&
+            (identical(other.salePrice, salePrice) ||
+                other.salePrice == salePrice) &&
+            (identical(other.quantity, quantity) ||
+                other.quantity == quantity) &&
+            (identical(other.distance, distance) ||
+                other.distance == distance) &&
+            (identical(other.saleStatus, saleStatus) ||
+                other.saleStatus == saleStatus));
+  }
+
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      storeId,
+      storeName,
+      const DeepCollectionEquality().hash(_imageUrl),
+      latitude,
+      longitude,
+      address,
+      goodsName,
+      startTime,
+      endTime,
+      originPrice,
+      discount,
+      salePrice,
+      quantity,
+      distance,
+      saleStatus);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$GoodImplCopyWith<_$GoodImpl> get copyWith =>
+      __$$GoodImplCopyWithImpl<_$GoodImpl>(this, _$identity);
+}
+
+abstract class _Good implements Good {
+  const factory _Good(
+      {required final String storeId,
+      required final String storeName,
+      required final List<String> imageUrl,
+      required final double latitude,
+      required final double longitude,
+      required final double address,
+      required final double goodsName,
+      required final String startTime,
+      required final String endTime,
+      required final int originPrice,
+      required final double discount,
+      required final double salePrice,
+      required final double quantity,
+      required final double distance,
+      required final double saleStatus}) = _$GoodImpl;
+
+  @override
+  String get storeId;
+  @override
+  String get storeName;
+  @override
+  List<String> get imageUrl;
+  @override
+  double get latitude;
+  @override
+  double get longitude;
+  @override
+  double get address;
+  @override
+  double get goodsName;
+  @override
+  String get startTime;
+  @override
+  String get endTime;
+  @override
+  int get originPrice;
+  @override
+  double get discount;
+  @override
+  double get salePrice;
+  @override
+  double get quantity;
+  @override
+  double get distance;
+  @override
+  double get saleStatus;
+  @override
+  @JsonKey(ignore: true)
+  _$$GoodImplCopyWith<_$GoodImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
