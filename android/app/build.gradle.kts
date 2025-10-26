84e234bcd0c5f5bc822e3ed36d5b70f6af00c3ae
kimwest00 <kimwest00@hufs.ac.kr>
2025-10-27 07:57:48 +0900

    add: 패키지명 변경, rebase 충돌 수정

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.trendflow.magambell"
+    namespace = "com.trendflow.bread"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.trendflow.magambell"
+        applicationId = "com.trendflow.bread"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- lib/main_dev.dart
+++
@@ -1,3 +0,0 @@
-import 'package:magambell/core/config/environment.dart';
-
-Future<void> main() async => Environment.newInstance(BuildType.dev).run();
