455fec77fa06b8af87b710214dd292278d267479
kimwest00 <kimwest00@hufs.ac.kr>
2025-10-29 21:52:18 +0900

    fix:textfield prefix,suffix icon 추가

--- lib/src/features/address/presentation/search_address_screen.dart
+++ lib/src/features/address/presentation/search_address_screen.dart
@@ -3,6 +3,8 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:magambell/src/constants/index.dart';
+import 'package:magambell/src/core/extensions/widget_extension.dart';
 import 'package:magambell/src/core/theme/mg_text_style.dart';
 import 'package:magambell/src/widgets/base_appbar.dart';
 import 'package:magambell/src/widgets/base_scaffold.dart';
@@ -23,31 +25,41 @@ class SearchAddressScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    log("SearchAddressScreen build");
-    return Listener(
-      onPointerDown: (event) => log('Pointer down at ${event.position}'),
-      child: BaseScaffold(
-        appBar: BaseAppBar(
-          title: Text("주소 설정"),
-          action: MgButton(onPressed: () {}, content: Text("편집").textGray()),
-        ),
-        body: SafeArea(
-          child: Column(
-            children: [
-              MgTextField(hintText: '지역의 동 이름으로 검색해보세요!'),
-              MgButton(onPressed: () {}, content: Text("현재 위치로 찾기")),
-            // Expanded(
-            //   child: ListView.builder(
-            //     itemCount: 0,
-            //     itemBuilder: (context, index) {
-            //       return SizedBox();
-            //     },
-            //   ),
-            // ),
-            ],
-          ),
+    return BaseScaffold(
+      appBar: BaseAppBar(
+        title: Text("주소 설정"),
+        action: MgButton(
+          onPressed: () {},
+          content: Text("편집").textGray().regular(),
         ),
       ),
+      body: Column(
+        children: [
+          // TODO: 페이지 켰을때 focusNode 자동 포커스되는지 확인
+          MgTextField(
+            hintText: '지역의 동 이름으로 검색해보세요!',
+            onSubmitted: (text) {
+              // TODO: 주소 검색 API 연동
+            },
+          ),
+          MgButton(
+            onPressed: () {
+              // TODO: 현재 위치로 찾기 기능 구현
+            },
+            content: Text("현재 위치로 찾기").xs().regular(),
+            padding: Gutter.hsm,
+          ).gray(),
+          // Expanded(
+          //   child: ListView.builder(
+          //     itemCount: 0,
+          //     itemBuilder: (context, index) {
+          // TODO: dataSource sharedPreference로 변경
+          //       return SizedBox();
+          //     },
+          //   ),
+          // ),
+        ],
+      ).margin(horizontal: MgSizes.md),
     );
   }
 }
