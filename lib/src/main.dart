0b114865483b61db405d789a2e1050e0045ed83f
kimwest00 <kimwest00@hufs.ac.kr>
2025-10-31 00:24:46 +0900

    feat: shorebird 업데이트로직 추가

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -3,13 +3,17 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:magambell/src/core/utils/shorebird_manager.dart';
 import 'package:magambell/src/main_app.dart';
 
 Future<void> runMagamBellApp() async {
-  // WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
 
   await runZonedGuarded<Future<void>>(() async {
     await dotenv.load(fileName: '.env');
+
+    // Shorebird 자동 업데이트 확인
+    ShorebirdManager.checkAndDownloadUpdate();
 
     runApp(const MagambellApp());
   }, (error, stackTrace) => log(error.toString()));
