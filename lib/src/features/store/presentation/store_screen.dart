aaa5d1fd5417716e6be53b851c04f4aa8e351caf
kimwest00 <kimwest00@hufs.ac.kr>
2025-11-01 21:56:02 +0900

    feat: 가게 정보 텍스트 구성, 이미지뷰 구성

--- lib/src/features/store/presentation/store_screen.dart
+++ lib/src/features/store/presentation/store_screen.dart
@@ -1,7 +1,10 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:magambell/src/constants/index.dart';
+import 'package:magambell/src/core/extensions/datetime_extension.dart';
 import 'package:magambell/src/core/extensions/price_extension.dart';
 import 'package:magambell/src/core/extensions/widget_extension.dart';
 import 'package:magambell/src/core/theme/mg_color.dart';
@@ -30,31 +33,73 @@ class DetailScreen extends ConsumerStatefulWidget {
   final String id;
 
   @override
-  State<DetailScreen> createState() => _DetailScreenState();
+  ConsumerState<DetailScreen> createState() => _DetailScreenState();
 }
 
-class _DetailScreenState extends State<DetailScreen> {
+class _DetailScreenState extends ConsumerState<DetailScreen> {
   @override
   Widget build(BuildContext context) {
     // final store = ref.watch(storeGoodsDetailProvider(id));
     final store = mockData;
     return BaseScaffold(
       appBar: BaseAppBar(title: Text('상세 화면 ${widget.id}')),
-      body: Center(
+      body: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [_buildStoreDescriptionSection(store)],
+          children: [
+            _buildThumbnailImageView(store),
+            _buildStoreDescriptionSection(store),
+            Divider(thickness: MgSizes.size6).margin(vertical: MgSizes.lg),
+            _buildStoreLocationInfoSection(store),
+          ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildThumbnailImageView(Goods store) {
+    if (store.ImageUrl.isEmpty) {
+      return Container(
+        height: 300,
+        color: MgColorScheme.gray2,
+        child: Center(child: Text('이미지 없음').textGray()),
+      );
+    }
+
+    return SizedBox(
+      height: 240,
+      child: Swiper(
+        autoplay: true,
+        autoplayDelay: 4000,
+        loop: store.ImageUrl.length > 1,
+        itemCount: store.ImageUrl.length,
+        pagination: SwiperPagination(
+          builder: DotSwiperPaginationBuilder(
+            activeColor: MgColorScheme.primary,
+            color: MgColorScheme.gray4,
+          ),
+        ),
+        itemBuilder: (context, index) {
+          final imageUrl = store.ImageUrl[index];
+          if (imageUrl.isEmpty) {
+            return Container(
+              color: MgColorScheme.gray2,
+              child: Center(child: Text('이미지 없음').textGray()),
+            );
+          }
+          return CachedNetworkImage(imageUrl: imageUrl, fit: BoxFit.cover);
+        },
       ),
     );
   }
 
   Widget _buildStoreDescriptionSection(Goods store) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Row(
           children: [
             StoreTags(quantity: store.quantity, saleStatus: store.saleStatus),
+            Spacer(),
             GestureDetector(
               onTap: () {
                 // TODO: 좋아요
@@ -63,9 +108,12 @@ class _DetailScreenState extends State<DetailScreen> {
             ),
           ],
         ),
+        Gaps.h12,
         Text(store.storeName).md().bold(),
-        // Text(store.description).md().bold().margin(top: MgSizes.size4, bottom: MgSizes.size8),
-        StoreTags(quantity: store.quantity, saleStatus: store.saleStatus),
+        Text("ㅇㅇㅇㅇ").regular().textGray().margin(
+          top: MgSizes.size4,
+          bottom: MgSizes.size8,
+        ), //store.description
         DefaultTextStyle(
           style: context.textTheme.titleLarge!,
           child: Row(
@@ -80,9 +128,31 @@ class _DetailScreenState extends State<DetailScreen> {
             ],
           ),
         ),
-        Text('할인율: ${store.discount}%'),
-        Text('거리: ${store.distance}'),
-        Text('픽업시간: ${store.startTime} ~ ${store.endTime}'),
+      ],
+    ).margin(all: MgSizes.md);
+  }
+
+  Widget _buildStoreLocationInfoSection(Goods store) {
+    return Column(
+      spacing: MgSizes.size8,
+      children: [
+        _buildStoreInfoItem(
+          '픽업시간',
+          "${store.startTime.convertTime() ?? ''} ~ ${store.endTime.convertTime() ?? ''}",
+        ),
+        _buildStoreInfoItem('주차안내', "TODO"),
+        _buildStoreInfoItem('가게 주소', store.address), //TODO: 복사 버튼 추가
+        // TODO: 지도 추가
+      ],
+    ).margin(horizontal: MgSizes.md);
+  }
+
+  Widget _buildStoreInfoItem(String label, String value) {
+    return Row(
+      children: [
+        Text(label).md().textGray().bold(),
+        Gaps.w12,
+        Text(value).md().regular(),
       ],
     );
   }
