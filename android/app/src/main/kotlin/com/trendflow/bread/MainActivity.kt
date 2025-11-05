89c523479ec46a496c01ec2737fa01b92fa258c3
kimwest00 <kimwest00@hufs.ac.kr>
2025-11-05 22:20:28 +0900

    feat: 네이버 로그인 로직 적용 / client id -> consumer id로 변경

--- android/app/src/main/kotlin/com/trendflow/bread/MainActivity.kt
+++ android/app/src/main/kotlin/com/trendflow/bread/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.trendflow.bread
 
-import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.android.FlutterFragmentActivity
 
 class MainActivity : FlutterFragmentActivity()
--- lib/src/features/auth/presenation/login_screen.dart
+++ lib/src/features/auth/presenation/login_screen.dart
@@ -1,24 +1,54 @@
+import 'package:flash/flash.dart';
+import 'package:flash/flash_helper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:magambell/src/constants/assets.dart';
 import 'package:magambell/src/constants/index.dart';
 import 'package:magambell/src/core/extensions/widget_extension.dart';
+import 'package:magambell/src/core/router/app_router.dart';
 import 'package:magambell/src/core/theme/mg_color.dart';
 import 'package:magambell/src/core/theme/mg_text_style.dart';
+import 'package:magambell/src/features/auth/presenation/login_screen.controller.dart';
 import 'package:magambell/src/widgets/base_scaffold.dart';
 import 'package:magambell/src/widgets/mg_button.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends ConsumerWidget {
   const LoginScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    // // Controller state 감지
+    // ref.listen<AsyncValue<void>>(loginControllerProvider, (previous, next) {
+    //   next.when(
+    //     data: (authResult) {
+    //     //   // 로그인 성공 시 메인 화면으로 이동
+    //     //   if (authResult == null) {
+    //     //     context.go(const MainRoute().location);
+    //     //   } else {
+    //     //     // 신규 회원 - 회원가입 화면으로 이동
+    //     //     context.push(const SelectUserTypeRoute().location);
+    //     //   }
+    //     // },
+    //     error: (error, stack) {
+    //       // 에러 토스트 표시
+    //       context.showErrorBar(content: Text(error.toString()));
+    //     },
+    //     loading: () {},
+    //   );
+    // });
+
+    final loginState = ref.watch(loginScreenControllerProvider);
+    final isLoading = loginState.isLoading;
+
     return BaseScaffold(
       body: SafeArea(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("BITE PICK").xxl().bold(),
+            Text("BITE PICK").xxl().bold(), // TODO: 로고 추가
             Gaps.h24,
+            if (isLoading) ...[const CircularProgressIndicator(), Gaps.h24],
             Column(
               spacing: MgSizes.sm,
               children: [
@@ -28,13 +58,19 @@ class LoginScreen extends StatelessWidget {
                   imagePath: R.ASSETS_IMAGES_KAKAO_LOGIN_PNG,
                   backgroundColor: MgColorScheme.primary,
                   textColor: MgColorScheme.text,
+                  isLoading: isLoading,
                 ),
                 _buildSocialLoginButton(
-                  onPressed: () {},
+                  onPressed: () async {
+                    await ref
+                        .read(loginScreenControllerProvider.notifier)
+                        .signInWithNaver();
+                  },
                   text: "네이버로 로그인",
                   imagePath: R.ASSETS_IMAGES_NAVER_LOGIN_PNG,
                   backgroundColor: const Color(0xFF36AE3C),
                   textColor: MgColorScheme.gray11,
+                  isLoading: isLoading,
                 ),
                 _buildSocialLoginButton(
                   onPressed: () {},
@@ -42,6 +78,7 @@ class LoginScreen extends StatelessWidget {
                   imagePath: R.ASSETS_IMAGES_APPLE_LOGIN_PNG,
                   backgroundColor: MgColorScheme.gray1,
                   textColor: MgColorScheme.gray11,
+                  isLoading: isLoading,
                 ),
               ],
             ),
@@ -57,9 +94,10 @@ class LoginScreen extends StatelessWidget {
     required String imagePath,
     required Color backgroundColor,
     required Color textColor,
+    required bool isLoading,
   }) {
     return MgButton(
-      onPressed: onPressed,
+      onPressed: isLoading ? null : onPressed,
       backgroundColor: backgroundColor,
       content: Row(
         mainAxisAlignment: MainAxisAlignment.center,
