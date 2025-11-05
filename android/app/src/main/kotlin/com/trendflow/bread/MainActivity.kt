772c4592d3f59f154bf5188571135567dadd9387
kimwest00 <kimwest00@hufs.ac.kr>
2025-11-05 20:56:14 +0900

    add: Nid 패키지 추가 및 native 설정, secret파일 추가

--- android/app/src/main/kotlin/com/trendflow/bread/MainActivity.kt
+++ android/app/src/main/kotlin/com/trendflow/bread/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.trendflow.bread
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
