f8082ff04528c6f6107449edf641d45870094060
kimwest00 <kimwest00@hufs.ac.kr>
2025-11-05 20:22:46 +0900

    feat: 사장님 회원가입시 필요한 경로 추가, redirect 함수 추가

--- lib/src/core/router/app_router.dart
+++ lib/src/core/router/app_router.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:magambell/src/features/address/presentation/search_address_screen.dart';
+import 'package:magambell/src/features/auth/presenation/login_screen.dart';
+import 'package:magambell/src/features/auth/presenation/owner/owner_join_info_register_screen.dart';
+import 'package:magambell/src/features/auth/presenation/select_user_type_screen.dart';
 import 'package:magambell/src/features/main/presentation/main_screen.dart';
 import 'package:magambell/src/features/map/presentation/store_map_screen.dart';
 import 'package:magambell/src/features/order/presentation/order_caution_screen.dart';
@@ -22,6 +25,29 @@ final appRouter = GoRouter(
   routes: $appRoutes,
 );
 
+@TypedGoRoute<LoginRoute>(
+  name: 'LoginRoute',
+  path: '/auth',
+  routes: [
+    TypedGoRoute<SelectUserTypeRoute>(
+      name: 'SelectUserTypeRoute',
+      path: 'select-user-type',
+    ),
+    TypedGoRoute<OwnerJoinInfoRoute>(
+      name: 'OwnerJoinInfoRoute',
+      path: 'owner/join/info',
+    ),
+  ],
+)
+class LoginRoute extends GoRouteData {
+  const LoginRoute();
+
+  @override
+  Widget build(BuildContext context, GoRouterState state) {
+    return const LoginScreen();
+  }
+}
+
 @TypedGoRoute<MainRoute>(
   name: 'MainRoute',
   path: '/',
@@ -29,12 +55,7 @@ final appRouter = GoRouter(
     TypedGoRoute<StoreRoute>(
       name: 'StoreRoute',
       path: 'store/:id',
-      routes: [
-        TypedGoRoute<StoreMapRoute>(
-          name: 'StoreMapRoute',
-          path: 'map',
-        ),
-      ],
+      routes: [TypedGoRoute<StoreMapRoute>(name: 'StoreMapRoute', path: 'map')],
     ),
     TypedGoRoute<SearchRoute>(name: 'SearchRoute', path: 'search'),
     TypedGoRoute<SearchAddressRoute>(
@@ -54,5 +75,19 @@ class MainRoute extends GoRouteData {
   @override
   Widget build(BuildContext context, GoRouterState state) {
     return const MainScreen();
+  }
+
+  @override
+  Future<String?> redirect(BuildContext context, GoRouterState state) async {
+    try {
+      return _checkRoute();
+    } catch (e) {
+      return LoginRoute().location;
+    }
+  }
+
+  Future<String> _checkRoute() async {
+    // TODO: auth Token manager에 token이 있다면 자동 로그인 시도
+    return LoginRoute().location;
   }
 }
