102a5032ecc344cd285cdec540fe6ea6d05b14e6
kimwest00 <kimwest00@hufs.ac.kr>
2025-12-06 13:21:04 +0900

    fix: kakao dev test앱->앱으로 변경 및 key교체

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -17,12 +17,22 @@ import NidThirdPartyLogin
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
   override func application(
-    _ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]
+    _ app: UIApplication,
+    open url: URL,
+    options: [UIApplication.OpenURLOptionsKey : Any] = [:]
   ) -> Bool {
-    if NidOAuth.shared.handleURL(url) == true {  // If the URL was passed from the Naver app
+
+    // 1) 네이버가 처리해야 하는 URL이면 네이버로 먼저 넘기고 종료
+    if NidOAuth.shared.handleURL(url) == true {
       return true
     }
-    // Handle URLs coming from other apps
+
+    // 2) 그 외 모든 URL은 Flutter쪽(플러그인들)에게 전달 (카카오, url_launcher 등)
+    if super.application(app, open: url, options: options) {
+      return true
+    }
+
+    // 3) 여기까지도 아무도 처리 안 했다면 false
     return false
   }
 }
