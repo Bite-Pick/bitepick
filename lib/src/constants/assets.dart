63b0607c56afe4c3bd13227538687a10e2bbd4e1
kimwest00 <kimwest00@hufs.ac.kr>
2025-12-20 20:25:48 +0900

    chore: 에셋 추가및 디렉토리 선언 오류 수정, 캐시 클린 명령어 추가

--- lib/src/constants/assets.dart
+++ lib/src/constants/assets.dart
@@ -267,6 +267,9 @@ class R {
   static const String ASSETS_IMAGES_APPLE_LOGIN_PNG =
       'assets/images/apple_login.png';
 
+  static const String ASSETS_IMAGES_CHARACTER_STANDING_PNG =
+      'assets/images/character/standing.png';
+
   static const String ASSETS_IMAGES_CHARACTER_1_PNG =
       'assets/images/character_1.png';
 
