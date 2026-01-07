955d0414e159b4dead42f00687bcc14fa9bf7fc6
kimwest00 <kimwest00@hufs.ac.kr>
2026-01-08 00:07:42 +0900

    fix: icon path 오탈자 수정

--- lib/src/widgets/base_svg_icon.dart
+++ lib/src/widgets/base_svg_icon.dart
@@ -162,7 +162,7 @@ class BaseSvgIcon extends StatelessWidget {
   factory BaseSvgIcon.homeLogo({double? size, Color? color}) =>
       _makeIcon('home_logo.svg', size, color);
         factory BaseSvgIcon.helpCirlce({double? size, Color? color}) =>
-      _makeIcon('help_circle.svg', size, color);
+      _makeIcon('help-circle.svg', size, color);
 
   BaseSvgIcon copyWith({Color? color, double? size, bool? hasColorFilter}) {
     return BaseSvgIcon(
